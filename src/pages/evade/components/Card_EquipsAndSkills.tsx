b640a7b6823b897e7c5cd7a52c1be6de1c493a94
henriquebg88 <henriquebg88@gmail.com>
2020-09-05 02:25:09 -0300

    Minor changes to cards layout

--- src/pages/evade/components/Card_EquipsAndSkills.tsx
+++ src/pages/evade/components/Card_EquipsAndSkills.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
 import img_equip from '../../../assets/sages_hat.png';
+import img_support from '../../../assets/support.png';
 
 const Card_EquipsAndSkills = () => {
-    return(
+    return (
         <div className="card">
             <div className="field equip">
                 <h1>Equip</h1>
-                <img src={img_equip}/>
+                <img src={img_equip} />
             </div>
             <div className="field skills">
                 <h1>Skills</h1>
+                <div className="field">
+                    <div className="field-inline">
+                        <img src={img_support} />
+                    </div>
+                    <div className="field-inline">
+                        <legend>Shadow Runner</legend>
+                        <p>AGI + 12%</p>
+                        <p>LUCK + 12%</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
