a0e63a54bffab896c9f036a6c07c3a8be0a68b44
henriquebg88 <henriquebg88@gmail.com>
2020-09-09 15:06:27 -0300

    Started working with data mining from git

--- src/pages/evade/index.tsx
+++ src/pages/evade/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 import Card_Char from './components/Card_Char';
 import Card_EquipsAndSkills from './components/Card_EquipsAndSkills';
@@ -7,7 +8,42 @@ import CardStatus from './components/Card_Status';
 import './styles.css';
 import './components/card.css';
 
+interface unitDatabase {
+    items: [{
+        iname: string,
+        charaId: string,
+        rare: number,
+        elem: number[],
+        jobsets: string[],
+        status:[
+            {},
+            {
+                hp: number,
+                dex: number,
+                atk: number,
+                def: number,
+                mag: number,
+                spd: number,
+                mov: number,
+                jmp: number,
+                luk: number,
+                ap: number
+            }
+        ]
+
+    }];
+}
+
 const Evade = () => {
+
+    useEffect( () => {
+        axios.get<unitDatabase>('https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/data/Unit.json').then( response => {
+            console.log(response.data.items.find( char => char.charaId == 'LAPSmont' ));
+            //https://github.com/shalzuth/wotv-ffbe-dump/blob/master/en/UnitName.json
+            //https://github.com/shalzuth/wotv-ffbe-dump/blob/master/en/JobName.json
+        })
+    }, [] )
+
     return (
         <div id="page-evade">
             <main>
