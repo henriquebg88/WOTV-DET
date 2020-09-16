2c3097850122a75f5b5d2ebcda13bdb13a70740a
henriquebg88 <henriquebg88@gmail.com>
2020-09-16 02:25:50 -0300

    .

--- web/src/pages/evade/index.tsx
+++ web/src/pages/evade/index.tsx
@@ -7,14 +7,18 @@ import CardStatus from './components/Card_Status';
 
 import './styles.css';
 import './components/card.css';
+import { Console } from "console";
+
 interface unitDatabase {
     items: [{
         iname: string,
         charaId: string,
         rare: number,
+        type: number,
         elem: number[],
         jobsets: string[],
-        status:[
+        piece: string,
+        status: [
             {},
             {
                 hp: number,
@@ -32,25 +36,89 @@ interface unitDatabase {
 
     }];
 }
+interface unitName {
+    infos:
+    [
+        {
+            key: string,
+            value: string
+        }
+    ]
+}
+interface unitNameData {
+    key: string,
+    value: string
+}
+interface units {
+    items: []
+}
 
 const Evade = () => {
 
-    useEffect( () => {
-        axios.get<unitDatabase>('https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/data/Unit.json').then( response => {
-            console.log(response.data.items.find( char => char.charaId == 'LAPSmont' ));
-            //https://github.com/shalzuth/wotv-ffbe-dump/blob/master/en/UnitName.json
-            //https://github.com/shalzuth/wotv-ffbe-dump/blob/master/en/JobName.json
+    useEffect(() => {
+        // https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/data/Unit.json
+        // https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/en/UnitName.json
+        // https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/en/JobName.json
+
+        // NOME DAS UNIDADES POR CHAVE
+        // axios.get<unitName>('https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/en/UnitName.json').then( response => {
+        //     console.log(response.data.infos.map( data => {
+        //         console.log(data);
+        //     }));
+        // })
+
+
+        axios.get<unitDatabase>('https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/data/Unit.json').then(response => {
+
+            const units: object[] = [];
+            const teste: any[] = [1, 2, 3, 5, 3, 56, 435];
+
+            //Get unit name using KEY                
+            response.data.items.map( item => {
+                //type 0 seems to refer to playable units
+                if (item.piece) {
+
+                    axios.get<unitName>('https://raw.githubusercontent.com/shalzuth/wotv-ffbe-dump/master/en/UnitName.json').then( response => {
+                        //Push esta indo depois de o array ser contabilizado??
+                        //Push esta indo depois de o array ser contabilizado??
+                        //Push esta indo depois de o array ser contabilizado??
+                        //Push esta indo depois de o array ser contabilizado??
+                        //Push esta indo depois de o array ser contabilizado??
+                        teste.push(1);
+                    })
+
+
+
+
+                }
+
+            });
+
+            console.log(units);
+            console.log(units.length)
+
+            units.map(() => {
+                console.log("oi");
+            })
+
+            console.log(teste);
+            teste.map(numero => {
+                console.log(numero)
+            })
+
+
         })
-    }, [] )
+
+    }, [])
 
     return (
         <div id="page-evade">
             <main>
                 <h1>Página de teste</h1>
                 <div className="container">
-                    <Card_Char/>
-                    <Card_EquipsAndSkills/>
-                    <CardStatus/>
+                    <Card_Char />
+                    <Card_EquipsAndSkills />
+                    <CardStatus />
                 </div>
             </main>
         </div>
